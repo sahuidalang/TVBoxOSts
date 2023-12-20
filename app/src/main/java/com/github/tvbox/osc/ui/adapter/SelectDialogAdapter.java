8884c2391ea594874b3ebb6c254b8c19ecac3443
sahuidalang <64364641+sahuidalang@users.noreply.github.com>
2023-12-20 21:38:52 +0800

    Update SelectDialogAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/SelectDialogAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/SelectDialogAdapter.java
@@ -89,7 +89,7 @@ public class SelectDialogAdapter<T> extends ListAdapter<T, SelectDialogAdapter.S
         {
             view.setTextColor(0xff02f8e1);
             view .setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-        }
+        };
         else 
            name = "○ " + name;
         {
